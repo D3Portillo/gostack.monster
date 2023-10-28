9e7e23a99a2e7621c1321644b5447ac55968b5d0
D3Portillo <d3portillo@gmail.com>
2023-10-28 08:48:34 +0200

    chores: init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import { Inter } from "next/font/google"
+
+const nextFont = Inter({ subsets: [], display: "swap" })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nextFont.className}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  )
 }
