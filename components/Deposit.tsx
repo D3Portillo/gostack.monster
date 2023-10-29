c9550ec28ac53ebb46fe964ce1248e36ce77d4a0
D3Portillo <d3portillo@gmail.com>
2023-10-29 05:29:34 +0100

    FEAT: update layout + enhance ui. #wip

---
+++ components/Deposit.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react"
+import btc from "@scure/btc-signer"
+import { bytesToHex, hexToBytes } from "@noble/hashes/utils"
+
+import useOnOffMachine from "@/lib/useOnOffMachine"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+import { HiSwitchHorizontal } from "react-icons/hi"
+
+function Deposit() {
+  const depositSAT = useOnOffMachine()
+  const [satoshis, setSatoshis] = useState(0)
+
+  const handleInputChange = (event: any) => {
+    setSatoshis(event.target.value)
+  }
+
+  const buildTransaction = async (e: any) => {
+    const { sbtcDepositHelper, TESTNET, TestnetHelper } = await import("sbtc")
+    e.preventDefault()
+
+    // Helper for working with various API and RPC endpoints and getting and processing data
+    // Change this depending on what network you are working with
+    const testnet = new TestnetHelper()
+    // const testnet = new DevEnvHelper();
+
+    // setting BTC address for devnet
+    // Because of some quirks with Leather, we need to pull our BTC wallet using the helper if we are on devnet
+    // const bitcoinAccountA = await testnet.getBitcoinAccount(WALLET_00);
+    // const btcAddress = bitcoinAccountA.wpkh.address;
+    // const btcPublicKey = bitcoinAccountA.publicKey.buffer.toString();
+
+    // setting BTC address for testnet
+    // here we are pulling directly from our authenticated wallet
+    const btcAddress = "userData.profile.btcAddress.p2wpkh.testnet"
+    const btcPublicKey = "userData.profile.btcPublicKey.p2wpkh"
+
+    let utxos = await testnet.fetchUtxos(btcAddress)
+
+    // If we are working via testnet
+    // get sBTC deposit address from bridge API
+    const response = await fetch(
+      "https://bridge.sbtc.tech/bridge-api/testnet/v1/sbtc/init-ui"
+    )
+    const data = await response.json()
+    const sbtcWalletAddress = data.sbtcContractData.sbtcWalletAddress
+
+    // if we are working via devnet we can use the helper to get the sbtc wallet address, which is associated with the first wallet
+    // const sbtcWalletAccount = await testnet.getBitcoinAccount(WALLET_00);
+    // const sbtcWalletAddress = sbtcWalletAccount.tr.address;
+
+    const tx = await sbtcDepositHelper({
+      // comment this line out if working via devnet
+      network: TESTNET,
+      stacksAddress: "",
+      amountSats: satoshis,
+
+      // we can use the helper to get an estimated fee for our transaction
+      feeRate: await testnet.estimateFeeRate("low"),
+      // the helper will automatically parse through these and use one or some as inputs
+      utxos,
+      // where we want our remainder to be sent. UTXOs can only be spent as is, not divided, so we need a new input with the difference between our UTXO and how much we want to send
+      bitcoinChangeAddress: btcAddress,
+    })
+
+    // convert the returned transaction object into a PSBT for Leather to use
+    const psbt = tx.toPSBT()
+    const requestParams = {
+      publicKey: btcPublicKey,
+      hex: bytesToHex(psbt),
+    }
+    // Call Leather API to sign the PSBT and finalize it
+    const txResponse = await (window as any)["btc"].request(
+      "signPsbt",
+      requestParams
+    )
+    const formattedTx = btc.Transaction.fromPSBT(
+      hexToBytes(txResponse.result.hex)
+    )
+    formattedTx.finalize()
+
+    // Broadcast it using the helper
+    const finalTx = await testnet.broadcastTx(formattedTx)
+
+    // Get the transaction ID
+    console.log(finalTx)
+  }
+
+  return (
+    <section className="border p-7 rounded-xl bg-gradient-to-tl from-stacks-purple/10 to-white">
+      <h2 className="text-2xl mb-2 font-bold">Deposit BTC, mint sBTC</h2>
+      <p className="text-sm text-black/80">
+        Please input a BTC amount to mint sBTC (minimum: 0.0001 BTC). It will be
+        minted to your logged-in account
+      </p>
+
+      <button
+        onClick={depositSAT.toggle}
+        className="flex items-center mt-8 gap-1 text-stacks-purple text-sm"
+      >
+        <span>Switch to {depositSAT.isOn ? "BTC" : "SAT"}</span>
+        <HiSwitchHorizontal />
+      </button>
+      <div className="flex flex-col gap-4 mt-2">
+        <Input
+          type="balance"
+          className="rounded-xl"
+          placeholder={depositSAT.isOn ? "1000 SAT" : "0.0001 BTC"}
+        />
+        <Button className="-mx-1 rounded-full h-14 text-xl bg-stacks-purple hover:bg-stacks-purple">
+          Confirm Deposit
+        </Button>
+      </div>
+    </section>
+  )
+}
+
+export default Deposit
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
+import Image from "next/image"
+import Deposit from "@/components/Deposit"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs"
+
+import asset_logo from "@/assets/logo.svg"
+
+const TABS = {
+  DEPOSIT: "DEPOSIT",
+  WITHDRAW: "WITHDRAW",
+}
 
 export default function Home() {
   return (
-    <main className="grid min-h-screen place-content-center">
-      <form className="border p-4 rounded-xl">
-        <div className="flex gap-2">
-          <Input type="balance" placeholder="0.0000 SATS" />
-          <Button type="submit">Deposit</Button>
-        </div>
-      </form>
+    <main className="container flex flex-col gap-8 items-center min-h-screen p-4">
+      <nav className="flex items-center justify-between w-full">
+        <Image src={asset_logo} alt="" />
+        <Button className="rounded-full">Connect Wallet</Button>
+      </nav>
+      <Tabs defaultValue={TABS.DEPOSIT} className="w-full max-w-md">
+        <TabsList className="grid bg-stacks-purple/5 border h-12 rounded-xl [&_button]:h-full [&_button]:rounded-lg w-full grid-cols-2">
+          <TabsTrigger className="font-semibold" value={TABS.DEPOSIT}>
+            Mint sBTC
+          </TabsTrigger>
+          <TabsTrigger className="font-semibold" value={TABS.WITHDRAW}>
+            Withdraw BTC
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value={TABS.DEPOSIT}>
+          <Deposit />
+        </TabsContent>
+        <TabsContent value={TABS.WITHDRAW}>
+          <Deposit />
+        </TabsContent>
+      </Tabs>
     </main>
   )
 }
